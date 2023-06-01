78224fc24a1cd9447db5bbaebb59c7408c340c3e
euodtech <euodootech@gmail.com>
2023-06-01 16:05:57 +0800

    image size reduction

--- src/pages/home.js
+++ src/pages/home.js
@@ -27,30 +27,30 @@ const Home = () =>{
                     </svg>
                 </div>
             </section>
-            <section  id="compilation" className="compilation" style={{minHeight:"100vh", paddingTop:"70px"}}>
-                <div className="row compilation pb-5 m-0">
-                    <div className="col-6 text-center">
-                        <img src={c1} class=" w-50 card-img-top" alt="compilation"/>
+            <section  id="compilation" className="compilation">
+                <div className="row compilation pt-5 pb-5 m-0">
+                    <div className="col-6 text-center mb-5" >
+                        <img src={c1} class=" w-25 card-img-top" alt="compilation"/>
                     </div>
-                    <div className="col-6 message text-center">
+                    <div className="col-6 message text-center mb-5 bg-secondary text-light" >
                         <h3>Hindi ako nagphophone sa work pero wala e, dumating ka sa buhay ko</h3>
                     </div>
-                    <div className="col-6 message text-center">
+                    <div className="col-6 message text-center mb-5 bg-warning text-light">
                         <h3>Ang saya saya ko with you so kapag hindi kita kasama or nakausap for just a day nanghihina ako &#128542;</h3>
                     </div>
-                    <div className="col-6 text-center">
-                        <img src={c2} class=" w-50 card-img-top" alt="compilation"/>
+                    <div className="col-6 text-center mb-5">
+                        <img src={c2} class=" w-25 card-img-top" alt="compilation"/>
                     </div>
-                    <div className="col-6 text-center">
-                        <img src={c3} class=" w-50 card-img-top" alt="compilation"/>
+                    <div className="col-6 text-center mb-5">
+                        <img src={c3} class=" w-25 card-img-top" alt="compilation"/>
                     </div>
-                    <div className="col-6 message text-center">
+                    <div className="col-6 message text-center mb-5 bg-success text-light">
                         <h3>I love seeing you enjoy simple stuff hehe, sana laging ganyan </h3>
                     </div>
-                    <div className="col-6 message text-center">
+                    <div className="col-6 message text-center mb-5 bg-info text-light">
                         <h3>Tama na drama baby ayoko maiyak hmp...</h3>
                     </div>
-                    <div className="col-6 text-center">
+                    <div className="col-6 text-center mb-5">
                         <img src={c4} class=" w-50 card-img-top" alt="compilation"/>
                     </div>
                 </div>
